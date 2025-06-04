197d33f605fa32a97b4ffd4b52ca18e763ed90db
RRREX <2554923496@qq.com>
2025-06-04 19:38:41 +0800

    Delete unnecessary files or folders

--- include/get_Elevation.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void Get_Elevation(double x, double y, double z, double sx, double sy, double sz, double& E, double& A);
--- include/mapping_function.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// 投影函数选择
-double get_mapping_function(double e, int mf_type);
